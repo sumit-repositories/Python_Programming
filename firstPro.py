00d0f76d253214d3431715c10fcd6351a3d56e9c
Sumit Manjhi <sumitmanjhi923@gmail.com>
2025-01-30 02:24:54 +0530

    first python program

---
+++ firstPro.py
@@ -0,0 +1,31 @@
+
+#Learning to print
+print("hi my name is Sumit Kumar Manjhi\n"
+      "I am from Ranchi Jharkhand")
+
+print(""" Hi I am Sumit Kumar Manjhi
+I am from Ranchi Jharkhand""")
+
+
+#single line comment
+"""
+Multiple line comment print("HI")
+"""
+
+#Learning to take input
+
+name=input("Enter your name : ")
+age=int(input("Enter your age : "))
+percentage=float(input("Enter your percentage : "))
+
+print(name)
+print(age)
+print(percentage)
+
+#Using Evalute
+
+exp1=eval(input("Enter the expression : "))
+print(exp1)
+
+exp2=eval(input("Enter the python code : "))
+print(exp2)
